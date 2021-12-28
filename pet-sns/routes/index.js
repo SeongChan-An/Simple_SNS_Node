0a372c77125c0254ecb02cf7357d5faca46b173a
tjscks0510 <tjscks0510@gmail.com>
2021-12-28 15:38:12 +0900

    [Title] 기본 레이아웃 라우터 설정 Setting the default router [Description] setting router (workflow) [Relevant] modified routes/index

--- pet-sns/routes/index.js
+++ pet-sns/routes/index.js
@@ -1,8 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const authRouter = require("./auth");
+const postsRouter = require("./posts");
 
 router.get("/", (req, res) => {
   res.render("index", { postList: [] });
 })
+
+router.use("/auth", authRouter);
+router.use("/posts", postsRouter);
 
 module.exports = router;
